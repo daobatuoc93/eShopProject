b60d5d13c8d6bd83ccde67aa2921d18b27164608
Pauldao <daobatuoc93@gmail.com>
2020-08-01 20:44:45 +0700

    Add project files.

---
+++ eShopProject/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace eShopProject.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
