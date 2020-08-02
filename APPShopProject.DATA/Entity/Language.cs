ae7db4b1ac82e65f5fb853136af8cc83ce3dd773
Pauldao <daobatuoc93@gmail.com>
2020-08-02 10:30:26 +0700

    Add Entities and Enums and DbContext

---
+++ APPShopProject.DATA/Entity/Language.cs
@@ -0,0 +1,11 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace APPShopProject.DATA.Entity
+{
+    public class Language
+    {
+
+    }
+}
---
+++ APPShopProject.DATA/Entity/Product.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace APPShopProject.DATA.Entity
+{
+ //   [Table("Products")] 
+    public class Product
+    {
+       
+        public int Id { get; set; }
+        public decimal  Price { get; set; }
+        public decimal  OriginalPrice { get; set; }
+        public int stock { get; set; }
+        public int View { get; set; }
+        public DateTime Datecreate { get; set; }
+//        [Required]
+        public string SeoAlias { get; set; }
+    }
+}
